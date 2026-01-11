33003346349565f6635ed58a63c151684a8723f9
NguyenPhuocDaiToan <daitoan2000@gmail.com>
2026-01-12 00:59:00 +0700

    update referral and UI

--- src/services/customer.service.ts
+++ src/services/customer.service.ts
@@ -1,4 +1,4 @@
-import { UpdateCustomerInput, Customer, ReferralPaginate } from "@/types";
+import { UpdateCustomerInput, Customer, CustomerPaginate } from "@/types";
 import { apiFetch } from "@/lib/api"; // Giả sử apiFetch là một hàm gọi API chuẩn.
 
 export const customerService = {
@@ -10,9 +10,9 @@ export const customerService = {
   },
   getReferrals: async (query: {
     [key: string]: any;
-  }): Promise<ReferralPaginate> => {
+  }): Promise<CustomerPaginate> => {
     const queryString = new URLSearchParams(query).toString();
     const url = `/customers/referral?${queryString}`;
-    return await apiFetch<ReferralPaginate>(url);
+    return await apiFetch<CustomerPaginate>(url);
   },
 };
